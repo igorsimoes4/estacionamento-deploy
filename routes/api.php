71eb84cc5ac2988172d6f5b40d8ddd000a92d1de
Igor Simoes da Silvera <igor01silveira@gmail.com>
2023-11-29 21:13:46 -0300

    Teste Route Api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/cars', CarsController::class);
+Route::get('/cars', [CarsController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
