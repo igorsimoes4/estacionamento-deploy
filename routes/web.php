5e9e42b270608d384c9d13c2a115323593adabc1
Igor Simoes da Silvera <igor01silveira@gmail.com>
2025-02-14 19:29:05 -0300

    Alterando Routes

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect(route('login'));
 });
-
+///////
 Route::get('login', function () {return view('login');})->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
