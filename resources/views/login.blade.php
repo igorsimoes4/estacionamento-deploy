df6805c70719fc03ad689d437fec7b591bcce1e5
Igor Simoes da Silvera <igor01silveira@gmail.com>
2025-02-17 02:00:10 -0300

    Alterando pagina de Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,108 +1,115 @@
 @extends('adminlte::page')
 
 @section('adminlte_css')
-    <!-- Adiciona o favicon -->
     <link rel="icon" type="image/png" href="{{ asset('img/LogoEstacionamento.png') }}">
-
     <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.css') }}" />
     <link rel="stylesheet" href="{{ asset('fontawesome-free/css/all.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
-
-    <!-- Inclui os estilos padrão do AdminLTE -->
     @parent
+    <style>
+        body {
+            height: 100vh;
+        }
+        .login-box {
+            background-color: rgb(0, 105, 217);
+            border-radius: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            width: 400px;
+        }
+        .card {
+            border-radius: 20px;
+        }
+        .card-body {
+            border-radius: 20px;
+        }
+        .input-group-text {
+            border-radius: 0px 10px 10px 0px !important;
+        }
+        .input-group input {
+            border-radius: 10px 0px 0px 10px !important;
+        }
+        button {
+            border-radius: 10px !important;
+        }
+    </style>
 @endsection
-<script src="{{ asset('js/jquery.min.js') }}"></script>
+
+@section('title', 'Login | Sistema de Estacionamento')
+
+@section('body')
+    <div class="login-box">
+        <div class="login-logo">
+            <img src="{{ asset('img/LogoEstacionamento.png') }}" height="50px" alt="Logo Estacionamento">
+        </div>
+
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Faça login para acessar o painel</p>
+
+                @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
+                <form action="{{ route('login') }}" method="post" autocomplete="off">
+                    @csrf
+
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                            placeholder="Email" value="{{ old('email') }}" autocomplete="off">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                            placeholder="Senha" autocomplete="off">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+                        </div>
+                    </div>
+                </form>
+
+                <div class="row mt-3">
+                    <div class="col-6">
+                        <a href="" class="text-center">Esqueci minha senha</a>
+                    </div>
+                    <div class="col-6 text-right">
+                        <a href="{{ route('register') }}" class="text-center">Criar uma conta</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('adminlte_js')
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
     <script src="{{ asset('overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
     <script src="{{ asset('popper/popper.min.js') }}"></script>
     <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-
-@section('plugins.Chartjs', true)
-
-@section('title', 'Painel | Login')
-
-@section('css')
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" />
-    <meta http-equiv="refresh" content="300">
-@endsection
-
-@section('js')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script>
-        @if (session('error'))
-            toastr.options = {
-                "closeButton": true,
-                "debug": false,
-                "newestOnTop": false,
-                "progressBar": true,
-                "positionClass": "toast-top-right",
-                "preventDuplicates": false,
-                "onclick": null,
-                "showDuration": "600",
-                "hideDuration": "1000",
-                "timeOut": "5000",
-                "extendedTimeOut": "1000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-            }
-            toastr.error('{{ session('error') }}');
-        @endif
-    </script>
-
-
-@endsection
-
-@section('body')
-    <div class="wrapper form active">
-        <header>
-            <img src="{{asset('img/LogoEstacionamento.png')}}"  height="50px">
-            <p>@lang('adminlte::adminlte.sign_in')</p>
-
-        </header>
-        <div class="form login">
-            <form action="{{ route('login') }}" method="post" autocomplete="off">
-                @csrf
-                <div class="input-group mb-3">
-                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                        placeholder="@lang("adminlte::adminlte.email")" value="{{ old('email') }}" autocomplete="off" >
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                        placeholder="@lang('adminlte::adminlte.password')" value="{{ old('password') }}" autocomplete="off">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="col">
-                    <button type="submit" class="btn btn-primary btn-block">@lang('adminlte::adminlte.sign_in')</button>
-                </div>
-            </form>
-        </div>
-        <!-- /.login-card-body -->
-    </div>
-    <!-- /.login-box -->
+    @parent
 @endsection
